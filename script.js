711ebe0579c9781a7a013be99188d8e72854b951
Edands <theedands@gmail.com>
2021-01-13 11:53:24 -0400

    edit/delete functions

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // TODO:
 //  1. Update/Edit option
+//  5. Make a button to switch back the display
+//  6. Refactor switchDisplay() to make it just one function
 //  3/2. Delete Option
 //  2. Implement moment.js for the dates
 //  3. Make it so when you try to update a note the placeholder becomes the older info
-//  4. Make it so you can export/import Notes easily
+//  4. Make it so you can export/import Notes out of the browser or collect the data
 
 // Generates a random unique id for new records
 const uid = function () {
@@ -31,11 +33,16 @@ function loadStoredNotes() {
 	}
 }
 
-//  Switches the display to add a new note
+//  Switches the display to add or edit notes
 
 function switchDisplay() {
 	document.querySelector("#notes").style.display = "none";
 	document.querySelector("#new-container").style.display = "block";
+}
+
+function switchEditDisplay() {
+	document.querySelector("#notes").style.display = "none";
+	document.querySelector("#update-container").style.display = "block";
 }
 
 // Generate new Note HTML And Save written note
@@ -83,12 +90,55 @@ function saveNote() {
 	document.querySelector("#new-container").style.display = "none";
 }
 
+parentElementID = "";
+
 function updateNote(caller) {
-	let parentElementID = caller.parentNode.id;
+	parentElementID = caller.parentNode.id;
+
+	switchEditDisplay();
+}
+
+function saveUpdatedNote() {
 	let noteToEdit = document.querySelector(`#${parentElementID}`);
+
+	let updatedNotetitle = document.querySelector("#updated-note-title").value;
+	let updatedNoteBody = document.querySelector("#updated-note-body").value;
+
+	let noteTitle = noteToEdit.querySelector(".title");
+	let noteContent = noteToEdit.querySelector(".content");
+	let noteDate = noteToEdit.querySelector(".date");
+
+	noteTitle.innerHTML = updatedNotetitle;
+	noteContent.innerHTML = updatedNoteBody;
+
+	// Update info in localStorage
+	console.log(parentElementID);
+	let newRecordHTML = noteToEdit.outerHTML;
+	console.log(newRecordHTML);
+	localStorage.setItem(parentElementID, newRecordHTML);
+	console.log(`${parentElementID} edited in local storage`);
+
+	// Reset parentElementID
+	parentElementID = "";
+
+	// Switching back the display
+	document.querySelector("#notes").style.display = "block";
+	document.querySelector("#update-container").style.display = "none";
+}
+
+function deleteNote(caller) {
+	let parentElement = caller.parentNode.id;
+
+	localStorage.removeItem(parentElement);
+	document.getElementById(parentElement).remove();
+
+	console.log(`Record ${parentElement} removed`);
 }
 
 // Event listeners
 
 document.querySelector("#create-note").addEventListener("click", switchDisplay);
 document.querySelector("#save-note").addEventListener("click", saveNote);
+document
+	.querySelector("#save-updated-note")
+	.addEventListener("click", saveUpdatedNote);
