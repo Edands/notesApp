25e21b877768282fde48d4e25349b5572526feb1
Edands <theedands@gmail.com>
2021-01-20 15:24:35 -0400

    dark-mode

--- resources/script.js
+++ resources/script.js
@@ -1,12 +1,11 @@
 // TODO:
-//  1. Make it so you can export/import Notes out of the browser or collect the data
-// 	2. DarkMode
-//  3. Consider using uid or not.
-
-// Generates a random unique id for new records
-const uid = function () {
-	return Date.now().toString(36) + Math.random().toString(36).substr(2);
-};
+//  0. Improve vanilla and dark-mode colour pallete and text display
+//  1. Make it so you can export/import notes out of the browser or collect the data.
+// 	2. BUG: When the text in the textarea is too big it does mess up with the display of the note.
+//  3. Make the app more responsive.
+//  4. Consider making the site auto refresh after being idle for a certain ammount of time so the moment().fromNow() gets updated
+//  5. Add a popup or something similar to confirm note deletion
+//  6. Remove all the console.log()
 
 // Load previously stored records in localStorage
 function loadStoredNotes() {
@@ -49,6 +48,11 @@ function switchDisplay() {
 	let caller = null;
 	let notesDisplay = document.querySelector("#notes");
 
+	// Switches the display showing either the container for new notes or the one for edits
+	// depending on who called the event
+
+	// BUG: There is a bug that makes the edit container stay locked
+	// after presing the goback button when the edit container is showing.
 	try {
 		caller = switchDisplay.caller.name;
 		console.log(`being called from ${caller}`);
@@ -73,18 +77,15 @@ function switchDisplay() {
 	}
 }
 
-// There is a bug that makes the edit container stay locked
-// after presing the goback button when the edit container is showing.
-
 // Generate new Note HTML And Save written note
 
 function saveNote() {
-	// Get and store the new values
+	// Gets and store the new values
 
 	let notetitle = document.querySelector("#new-note-title").value;
 	let noteBody = document.querySelector("#new-note-body").value;
 
-	// Clone the html template for new notes
+	// Clones the html template for new notes
 
 	let noteTemplate = document.getElementById("noteTemplate");
 	let noteClone = noteTemplate.cloneNode(true);
@@ -92,13 +93,13 @@ function saveNote() {
 	sotoredNotes.appendChild(noteClone);
 	let newNote = sotoredNotes.lastChild;
 
-	// Catching the new note varibles
+	// Catchs the new note varibles
 
 	let newNoteTitle = newNote.querySelector(".title");
 	let newNoteContent = newNote.querySelector(".content");
 	let newNoteDate = newNote.querySelector(".date");
 
-	// Setting up the new values
+	// Sets up the new values
 
 	newNoteTitle.innerHTML = `${notetitle}`;
 	newNoteContent.innerHTML = `${noteBody}`;
@@ -110,7 +111,7 @@ function saveNote() {
 
 	newNote.style.setProperty("display", "inline-block");
 
-	//Save to localStorage
+	//Saves to localStorage
 
 	console.log(newNoteId);
 	let newRecordHTML = document.getElementById(String(newNoteId)).outerHTML;
@@ -122,16 +123,20 @@ function saveNote() {
 	switchDisplay();
 }
 
+// Inserts the specified node after an existing one
+
 function insertAfter(newNode, existingNode) {
 	existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
 }
 
+// This varibale stores the id of the element wich is going to be edited
 parentElementID = "";
 
 function updateNote(caller) {
+	// Set up the id of the note witch is going to be edited
 	parentElementID = caller.parentNode.id;
 
-	// Showing the text in your older note about to edit
+	// This Shows the text of the note wich is going to be edited in the edit container
 
 	let noteTitle = document
 		.getElementById(`${parentElementID}`)
@@ -145,12 +150,17 @@ function updateNote(caller) {
 
 	document.getElementById("updated-note-body").innerHTML = noteBody;
 
-	// Switching back the display
+	// Switches to the edit container
 	switchDisplay();
 }
 
+// Updates the info of an edited note
+
 function saveUpdatedNote() {
+	// The current target note
 	let noteToEdit = document.getElementById(`${parentElementID}`);
+
+	// Getting all the new values
 
 	let updatedNotetitle = document.querySelector("#updated-note-title").value;
 	let updatedNoteBody = document.querySelector("#updated-note-body").value;
@@ -165,20 +175,23 @@ function saveUpdatedNote() {
 	noteTitle.innerHTML = updatedNotetitle;
 	noteContent.innerHTML = updatedNoteBody;
 
+	// Creates a unix timestamp with the current time
 	let momentNow = moment().valueOf();
 
-	noteDate.innerHTML = `Created ${moment(
+	// Updates the moment().fromNow() in the html
+	noteDate.innerHTML = `created ${moment(
 		parseInt(parentElementID)
 	).fromNow()}.`;
 
+	// Sets up a new <span> wich shows when was the last time the note was edited
 	if (noteUpDate) {
-		noteUpDate.innerHTML = `Last edited ${moment(momentNow).fromNow()}.`;
+		noteUpDate.innerHTML = `last edited ${moment(momentNow).fromNow()}.`;
 	} else {
-		dateSpan.innerHTML = `Last edited ${moment(momentNow).fromNow()}.`;
+		dateSpan.innerHTML = `last edited ${moment(momentNow).fromNow()}.`;
 		dateSpan.className = "last-edited";
 		dateSpan.id = momentNow;
-		insertAfter(breakEl, noteDate);
 		insertAfter(dateSpan, noteDate);
+		insertAfter(breakEl, noteDate);
 	}
 
 	// Updating info in localStorage
@@ -188,14 +201,15 @@ function saveUpdatedNote() {
 	localStorage.setItem(parentElementID, newRecordHTML);
 	console.log(`${parentElementID} edited in local storage`);
 
-	// Reset parentElementID
+	// Resets parentElementID
 	parentElementID = "";
 
-	// Switching back the display
+	// Switches back the display
 	switchDisplay();
 }
 
 // Deletes the note from both the DOM and the localStorage
+
 function deleteNote(caller) {
 	let parentElement = caller.parentNode.id;
 
@@ -205,12 +219,13 @@ function deleteNote(caller) {
 	console.log(`Record ${parentElement} removed`);
 }
 
+// Updates the moment().fromNow() dates showed in each note after a page refresh
+
 function loadDates() {
 	let notesDiv = document.getElementById("notes");
 	let notesChilds = notesDiv.getElementsByTagName("a");
 
-	// Change it to i = 1 for production
-	for (i = 2; i < notesChilds.length; i++) {
+	for (i = 1; i < notesChilds.length; i++) {
 		let currentChild = notesChilds[i];
 		let childID = currentChild.id;
 		let childCreation = currentChild.querySelector(".date");
@@ -219,18 +234,39 @@ function loadDates() {
 			let childLastEdit = currentChild.querySelector(".last-edited");
 			let lastEditId = childLastEdit.id;
 			let editedDate = moment(parseInt(lastEditId));
-			childLastEdit.innerHTML = moment(editedDate).fromNow();
+			childLastEdit.innerHTML = `last edited ${moment(editedDate).fromNow()}`;
 		} else {
 			console.log(`there is no .last-edit in ${childID}`);
 		}
 
 		let creationDate = moment(parseInt(childID));
-		childCreation.innerHTML = moment(creationDate).fromNow();
+		childCreation.innerHTML = `created ${moment(creationDate).fromNow()} `;
+	}
+}
+
+// Controls the switch of classes for dark mode
+
+function darkModeSwitch() {
+	let moonSwitch = document.querySelector(".moon-icon");
+	let sunSwitch = document.querySelector(".sun-icon");
+	let bodyClass = document.body.classList;
+
+	if (!bodyClass.contains("dark-mode")) {
+		bodyClass.add("dark-mode");
+		moonSwitch.style.setProperty("display", "none");
+		sunSwitch.style.setProperty("display", "block");
+	} else {
+		bodyClass.remove("dark-mode");
+		moonSwitch.style.setProperty("display", "block");
+		sunSwitch.style.setProperty("display", "none");
 	}
 }
 
 // Event listeners
 
+document
+	.querySelector(".dark-mode-btn")
+	.addEventListener("click", darkModeSwitch);
 document.querySelector("#create-note").addEventListener("click", switchDisplay);
 document.querySelector("#save-note").addEventListener("click", saveNote);
 document
